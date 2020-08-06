b42b5481789dc5283181b5a88a8ba58974ed9548
aldair cerdeira <aldaircerdeira10@hotmail.com>
2020-08-05 23:21:04 -0400

    criação da home

---
+++ resources/views/tema/template.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title></title>
+
+  <!-- Custom fonts for this template-->
+  <link href="{{ asset('assets/startbootstrap/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="{{ asset('assets/startbootstrap/css/sb-admin-2.css')}}"   rel="stylesheet">
+  
+  <!-- favicon --->
+  <link  rel="shortcut icon" href="{{asset('/assets/img/favicon.ico') }}" type="image/x-icon"/>
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper" >
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column" >
+
+      <!-- Main Content -->
+      <div id="content" style="height: 109; padding: 0px; margin: 0px">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light topbar mb-4 static-top shadow" style="background: #363636 0% 0% no-repeat padding-box;height: 109px;opacity: 1;">
+
+          <ul class="navbar-nav" style="margin: 0 40% 0 40%">
+            <div style="height: 109px; width: 400px;">
+                <a style="height: 100%; float:left" >
+                <div style="max-width: 200px" >
+                    <img src="{{ asset('assets/img/Imagem_3.png')}}" width="100%">
+                </div>
+                </a>
+                <p style="text-align: left;font: Bold 23px/36px Montserrat;letter-spacing: 0.68px;color: #FFFFFF;text-transform: uppercase;opacity: 1; height: 30px;margin-top: 35px">
+                    nome 
+                </p>
+            </div>
+
+          </ul>
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+            
+
+            <!-- Nav Item - User Information -->
+            @if(isset(Auth::user()->name))
+                <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
+                    <img class="img-profile rounded-circle" src="{{asset('assets/img/avatar.png')}}">
+                </a>
+                <!-- Dropdown - User Information -->
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                    
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Sair
+                    </a>
+                </div>
+                </li>
+            @endif
+          </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+       
+
+          <!-- Page Heading 
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+
+          </div>-->
+            <!-- Bootstrap core JavaScript-->
+              <script src={{ asset('assets/startbootstrap/vendor/jquery/jquery.min.js') }}></script>
+              <script src={{ asset('assets/startbootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js') }}></script>
+              <script src={{ asset('assets/startbootstrap/vendor/jquery-mask/jquery.mask.min.js') }}></script>
+
+              <!-- Core plugin JavaScript-->
+              <script src={{ asset('assets/startbootstrap/vendor/jquery-easing/jquery.easing.js') }}></script>
+
+              <!-- Custom scripts for all pages-->
+              <script src={{ asset('assets/startbootstrap/js/sb-admin-2.min.js') }}></script>
+
+              <!-- Page level plugins -->
+              <script src={{ asset('assets/startbootstrap/vendor/chart.js/Chart.min.js') }}></script>
+
+              <!-- Page level custom scripts -->
+              <script src={{ asset('assets/startbootstrap/js/demo/chart-area-demo.js') }}></script>
+              <script src={{ asset('assets/startbootstrap/js/demo/chart-pie-demo.js') }}></script>
+            
+          
+            @yield('home')
+
+          <!-------------------------------------->
+          
+         
+
+      
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+     
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+  
+
+  <!-- Scroll to Top Button-->
+
+  
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Tem certeza?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Deseja realmente encerra sua sessão.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+          <form id="btn btn-primary" action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-primary">
+                {{ __('Sair') }}
+            </button>
+        </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+</body>
+ <!-- Footer -->
+ <footer style="top: 2969px;
+    left: 0px;
+    margin-bottom: 0px;
+    height: 410px;
+    background: #363636 0% 0% no-repeat padding-box;
+    opacity: 1;" >
+      <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+      </a>
+</footer>
+<!-- End of Footer -->
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,89 @@
-@extends('tema.painel_template')
+@extends('tema.template')
     
-@section('painel-admin')
+@section('home')
     
+    <div style="top: 0px;
+    left: 0px;
+    margin-top: -25px;
+    width: 100%;
+    height: 1080px;
+    background: transparent url('{{ asset('assets/img/Imagem 2.png')}}') 0% 0% no-repeat padding-box;
+    opacity: 0.85;">
+    
+        <div style="top: 0px;
+        margin: 0 30% 0 30%;
+        width: 586px;
+        height: 1080px;
+        background: transparent url('{{ asset('assets/img/Imagem card1.svc')}}') 0% 0% no-repeat padding-box;
+        box-shadow: 0px 3px 6px #00000099;
+        opacity: 1;">
+            <div style="width: 526px;
+                margin: 0px auto 0px auto;
+                height: 754px;
+                background: transparent url('{{ asset('assets/img/Imagem card.png')}}') 0% 0% no-repeat padding-box;
+                box-shadow: 0px 3px 6px #00000099;
+                border-radius: 196px;
+                opacity: 1;">
 
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
+            <p style="text-align: center;
+                font: Regular 20px/21px Open Sans;
+                padding: 0 30px 0 30px;
+                letter-spacing: 0px;
+                color: #F0F0F2;
+                text-shadow: 0px 3px 6px #00000029;
+                opacity: 1;"
+            >
+                ja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+
+                Porque nós o usamos?
+                É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).
+                
+                    
+            </p>
+
         </div>
-    </body>
+    </div>
+    
+    <div style="top: 1080px;
+    left: 0px;
+    width: 1920px;
+    height: 1147px;
+    background: #363636 0% 0% no-repeat padding-box;
+    opacity: 1;">
+    </div>
+
+    <div style="
+        left: 0px;
+        width: 1920px;
+        height: 700px;
+        background: transparent linear-gradient(143deg, #7DEDE2 0%, #58B790 100%) 0% 0% no-repeat padding-box;
+        opacity: 1;">
+        <div style="
+                margin: 0 20% 0 20%;
+                width: 700px;
+                height: 700px;
+                background: #FFFFFF 0% 0% no-repeat padding-box;
+                box-shadow: 0px 0px 6px #0000004D;
+                opacity: 1;">
+                <p style="
+                height: 43px;
+                text-align: center;
+                font: Bold 35px/46px Montserrat;
+                letter-spacing: -0.88px;
+                color: #63C7A9;
+                opacity: 1;">
+                FORMULARIO
+                </p>
+                <P style="text-align: left;
+                padding: 30px;
+                font: Regular 20px/21px Open Sans;
+                letter-spacing: 0px;
+                color: #363636;
+                opacity: 1;">
+                    ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).
+                </P>
+        </div>
+    </div>
 
 @endsection
